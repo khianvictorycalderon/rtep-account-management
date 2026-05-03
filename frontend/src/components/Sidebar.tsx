c8e4a597f2bc864a999d133eb6adff262f55a455
khianvictorycalderon <ckhianvictory@gmail.com>
2026-05-03 21:14:12 +0800

    Added logout cursor-pointer

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -100,6 +100,7 @@ export default function Sidebar({ open, setOpen }: Props) {
                                     key={`logout-${idx}`}
                                     onClick={handleLogout}
                                     className="
+                                        cursor-pointer
                                         w-full text-left flex items-center gap-2 px-3 py-2 rounded-md text-sm
                                         text-red-500 hover:bg-red-100 dark:hover:bg-red-900/30
                                     "
